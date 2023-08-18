7f4a7910cf3922758611d123283a1531307e773c
BrianDelalex <brian.delalex@outlook.com>
2023-08-18 09:29:09 +0200

    Adding http request test on /hello route.

--- back/src/test/java/com/medhead/POC/PocApplicationTests.java
+++ back/src/test/java/com/medhead/POC/PocApplicationTests.java
@@ -2,14 +2,21 @@ package com.medhead.POC;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PocApplicationTests {
 
+/**
+ * Sanity check test.
+ */
     @Autowired
     private PocApplication controller;
 
@@ -18,4 +25,19 @@ class PocApplicationTests {
         assertThat(controller).isNotNull();
     }
 
+/**
+ * Testing http route.
+ */
+    @LocalServerPort
+    private int port;
+
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void helloShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello",
+                String.class)).contains("Hello World!");
+    }
 }
