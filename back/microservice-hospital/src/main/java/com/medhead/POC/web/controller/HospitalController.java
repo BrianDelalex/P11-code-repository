2a5717f58cc4420f9fd9b61f03da262d085832ef
BrianDelalex <brian.delalex@outlook.com>
2023-12-07 09:38:07 +0100

    Removing unused console logs. Removing unused import.

--- back/microservice-hospital/src/main/java/com/medhead/POC/web/controller/HospitalController.java
+++ back/microservice-hospital/src/main/java/com/medhead/POC/web/controller/HospitalController.java
@@ -1,23 +1,15 @@
 package com.medhead.POC.web.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +21,6 @@ import com.medhead.POC.dao.HospitalRepository;
 import com.medhead.POC.dao.SpecialityRepository;
 import com.medhead.POC.models.Hospital;
 import com.medhead.POC.models.Speciality;
-import com.medhead.POC.models.MatrixApiTypes.Response;
 
 @RestController
 @CrossOrigin(maxAge = 3600)
@@ -77,64 +68,6 @@ public class HospitalController {
 
         data.put("data", result.getOrganisationName());
         return new ResponseEntity<Map<String, String>>(data, null, HttpStatus.OK);
-        // List<String> specialities = new ArrayList<String>();
-        // specialities.add(speciality);
-        // List<Hospital> hospitals = repository.findBySpecialitiesIsContaining(specialities);
-        // List<Response> responses = new ArrayList<Response>();
-
-        // hospitals = hospitals
-        //     .stream()
-        //     .filter(h -> h.getFreeBeds() > 0)
-        //     .collect(Collectors.toList());
-
-        
-        // long start = System.currentTimeMillis();
-        // for (Hospital h : hospitals) {
-        //     if (!hospitals.isEmpty()) {
-        //         HttpHeaders headers = new HttpHeaders();
-        //         List<MediaType> mediaTypes = new ArrayList<MediaType>();;
-        //         mediaTypes.add(MediaType.ALL);
-        //         headers.setAccept(mediaTypes);
-        //         String url = "https://maps.googleapis.com/maps/api/distancematrix/json?destinations={destinations}&origins={origins}&key={key}";
-
-        //         Map<String, String> params = new HashMap<>(Collections.singletonMap("destinations", h.getLatitude() + "," + h.getLongitude()));
-        //         params.put("origins", latitude + "," + longitude);
-        //         params.put("key", "AIzaSyCs6HGKrAjFRAzM_B3dVS50LrKcxboru54");
-
-        //         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(null, headers);
-        //         HttpEntity<Response> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Response.class, params);
-
-        //         responses.add(response.getBody());
-        //     }
-        // }
-        // long time = System.currentTimeMillis() - start;
-        // System.out.println("duration " + time + " ms");
-
-        // Response nearest = responses
-        //     .stream()
-        //     .min(Comparator.comparing(Response::GetDuration))
-        //     .orElse(null);
-        // Hospital result = null;
-        // if (nearest != null) {
-        //     int i = 0;
-        //     for (Response r : responses) {
-        //         if (r == nearest) {
-        //             result = hospitals.get(i);
-        //             break;
-        //         }
-        //         i++;
-        //     }
-        // }
-
-        // Map<String, String> data = new HashMap<>();
-        // data.put("data", "not_found");
-
-        // if (result == null) {
-        //     return new ResponseEntity<Map<String, String>>(data, null, HttpStatus.OK);
-        // }
-
-        // data.put("data", result.getOrganisationName());
-        // return new ResponseEntity<Map<String, String>>(data, null, HttpStatus.OK);
     }
 
      /** GET Route at /hospital/specialities.
